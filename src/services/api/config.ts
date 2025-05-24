7599e5655611305e54ec688e30e0eb0b1a53cb41
Kaan D <kaan_d_tr@hotmail.com>
2025-05-24 03:22:17 +0300

    initial commit.

---
+++ src/services/api/config.ts
@@ -0,0 +1,12 @@
+"use client";
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+// Original endpoints
+export const AUTH_REFRESH_URL = API_URL + "/v1/auth/refresh";
+export const AUTH_ME_URL = API_URL + "/v1/auth/me";
+export const AUTH_LOGOUT_URL = API_URL + "/v1/auth/logout";
+
+// Custom FastAPI endpoints
+export const CUSTOM_AUTH_LOGIN_URL = API_URL + "/auth/login";
+export const CUSTOM_AUTH_ME_URL = API_URL + "/auth/me";
+export const CUSTOM_AUTH_LOGOUT_URL = API_URL + "/auth/logout";
